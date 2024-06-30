f5daa09ff189238169b08956ee1184c11607b7be
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2024-06-30 08:12:48 -0400

    Initial commit.

---
+++ Private/Get/Get-PugForest.ps1
@@ -0,0 +1,12 @@
+function Get-PugForest {
+    [CmdletBinding()]
+    param(
+        [string]$ForestName
+    )
+
+    #requires -Version 5
+
+    $PugForest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+
+    $PugForest
+}
