22d4496fc7cf20ddbcb5f89b6e14fa29832ea8d2
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2024-08-18 18:31:02 -0400

    It's real now.

--- Private/Show/Show-PPLogo.ps1
+++ Private/Show/Show-PPLogo.ps1
@@ -18,6 +18,62 @@ function Show-PPLogo {
         [string]$Version
     )
 
-   $OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
-   Write-Host "PowerPUG! v<ModuleVersion>" -ForegroundColor Magenta
+    # Write-Host '' -BackgroundColor Black -ForegroundColor Red -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor DarkYellow -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor Yellow -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor Green -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor DarkGreen -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor Blue -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor DarkBlue -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor Magenta -NoNewline
+    # Write-Host '' -BackgroundColor Black -ForegroundColor DarkMagenta -NoNewline
+    # Write-Host
+
+    Write-Host '                                                                '
+
+    Write-Host '  ______ ' -BackgroundColor Black -ForegroundColor Red -NoNewline
+    Write-Host '       ' -BackgroundColor Black -ForegroundColor DarkYellow -NoNewline
+    Write-Host '         ' -BackgroundColor Black -ForegroundColor Yellow -NoNewline
+    Write-Host '      ' -BackgroundColor Black -ForegroundColor Green -NoNewline
+    Write-Host '     ' -BackgroundColor Black -ForegroundColor DarkGreen -NoNewline
+    Write-Host ' ______ ' -BackgroundColor Black -ForegroundColor Blue -NoNewline
+    Write-Host '_______ ' -BackgroundColor Black -ForegroundColor DarkBlue -NoNewline
+    Write-Host '_______ ' -BackgroundColor Black -ForegroundColor Magenta -NoNewline
+    Write-Host '__  ' -BackgroundColor Black -ForegroundColor DarkMagenta -NoNewline
+    Write-Host
+    
+    Write-Host ' |   __ \' -BackgroundColor Black -ForegroundColor Red -NoNewline
+    Write-Host '.-----.' -BackgroundColor Black -ForegroundColor DarkYellow -NoNewline
+    Write-Host '--.--.--.' -BackgroundColor Black -ForegroundColor Yellow -NoNewline
+    Write-Host '-----.' -BackgroundColor Black -ForegroundColor Green -NoNewline
+    Write-Host '----.' -BackgroundColor Black -ForegroundColor DarkGreen -NoNewline
+    Write-Host '|   __ \' -BackgroundColor Black -ForegroundColor Blue -NoNewline
+    Write-Host '   |   |' -BackgroundColor Black -ForegroundColor DarkBlue -NoNewline
+    Write-Host '     __|' -BackgroundColor Black -ForegroundColor Magenta -NoNewline
+    Write-Host '  | ' -BackgroundColor Black -ForegroundColor DarkMagenta -NoNewline
+    Write-Host
+
+    Write-Host ' |    __/' -BackgroundColor Black -ForegroundColor Red -NoNewline
+    Write-Host '|  _  |' -BackgroundColor Black -ForegroundColor DarkYellow -NoNewline
+    Write-Host '  |  |  |' -BackgroundColor Black -ForegroundColor Yellow -NoNewline
+    Write-Host '  -__|' -BackgroundColor Black -ForegroundColor Green -NoNewline
+    Write-Host '   _|' -BackgroundColor Black -ForegroundColor DarkGreen -NoNewline
+    Write-Host '|    __/' -BackgroundColor Black -ForegroundColor Blue -NoNewline
+    Write-Host '   |   |' -BackgroundColor Black -ForegroundColor DarkBlue -NoNewline
+    Write-Host '    |  |' -BackgroundColor Black -ForegroundColor Magenta -NoNewline
+    Write-Host '__| ' -BackgroundColor Black -ForegroundColor DarkMagenta -NoNewline
+    Write-Host
+
+    Write-Host ' |___|   ' -BackgroundColor Black -ForegroundColor Red -NoNewline
+    Write-Host '|_____|' -BackgroundColor Black -ForegroundColor DarkYellow -NoNewline
+    Write-Host '________|' -BackgroundColor Black -ForegroundColor Yellow -NoNewline
+    Write-Host '_____|' -BackgroundColor Black -ForegroundColor Green -NoNewline
+    Write-Host '__|  ' -BackgroundColor Black -ForegroundColor DarkGreen -NoNewline
+    Write-Host '|___|  ' -BackgroundColor Black -ForegroundColor Blue -NoNewline
+    Write-Host '|_______|' -BackgroundColor Black -ForegroundColor DarkBlue -NoNewline
+    Write-Host '_______|' -BackgroundColor Black -ForegroundColor Magenta -NoNewline
+    Write-Host '__| ' -BackgroundColor Black -ForegroundColor DarkMagenta -NoNewline
+    Write-Host
+    Write-Host "                                    v<ModuleVersion>    " -BackgroundColor Black -ForegroundColor Magenta
+    Write-Host '                                                        ' -BackgroundColor Black
 }
